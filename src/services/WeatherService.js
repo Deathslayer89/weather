27c4e3fb0574edd0d7c5f85d0444186cb40f5ebd
Dinesh kumar <72866547+Deathslayer89@users.noreply.github.com>
2022-09-14 23:22:37 +0530

    Update WeatherService.js
    updated env var

--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 
-const API_KEY = REACT_APP_process.env.API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = "https://api.openweathermap.org/data/2.5/";
 const getWeatherData = (infoType, searchParams) => {
   const url = new URL(BASE_URL + infoType);
